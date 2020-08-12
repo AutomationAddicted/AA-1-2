ca117c5f83c7e1c09108573ce8167941b7bf02ef
Automation Addicted <69575422+AutomationAddicted@users.noreply.github.com>
2020-08-12 10:38:00 +0200

    Create Calcolo_varianzaR.m

---
+++ Calcolo_varianzaR.m
@@ -0,0 +1,28 @@
+clc
+clear all;
+global a;
+
+% ricerca della porta COM per Arduino
+if ~isempty(instrfind)
+    fclose(instrfind);
+    delete(instrfind);
+end
+
+% creazione oggetto Arduino
+a=arduino();
+
+% numero di campioni da leggere
+samples=600;
+
+% inizializzazione vettore campioni
+v = zeros(samples,1);
+
+% lettura da sensore LM35DZ
+i=1;
+while i <= samples
+    v(i) = readVoltage(a,'A0')*100;
+    i = i + 1;
+end
+
+% calcolo varianza sul rumore di misura R
+R = var(v)
